734e580cde81bfa84686a2751ff5a65f89375506
adib sadeghi <adibsadeghi7272@gmail.com>
2021-12-06 13:57:08 +0330

    routes and pages was added to the project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-
-
-import './App.css';
-import Range from './components/Range';
+import "./App.css";
+import Range from "./components/Range";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./components/Auth/Signup/Signup";
+import Login from "./components/Auth/Login/Login";
 
 function App() {
   return (
     <div className="App">
- <div className="range">
- <Range/>
- </div>
+     <Routes>
+     <Route path="/" element={<Home />}/>
+     <Route path="/signup" element={<Signup />}/>
+     <Route path="/login" element={<Login />}/>
+     </Routes>
     </div>
   );
 }
