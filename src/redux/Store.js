3207c1f77b1e616c167c25ca474a4849c2a9e416
adib sadeghi <adibsadeghi7272@gmail.com>
2021-12-08 16:43:48 +0330

    reducer was created to fetch all products from json server

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import AuthReducer from "./reducers/AuthReducer"
+import ProductReducer from './reducers/ProductReducer'
 
 export const store = configureStore({
     reducer: {
-     auth:AuthReducer
+     auth:AuthReducer,
+     products:ProductReducer
     },
    
   })
---
+++ src/redux/reducers/ProductReducer.js
@@ -0,0 +1,46 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchProducts = createAsyncThunk(
+  "all/products",
+  async ({ rejectWithValue }) => {
+    try {
+      const { data } = await axios.get("http://localhost:4000/products");
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.response.data.message);
+    }
+  }
+);
+
+const productsSlice = createSlice({
+    name: "products",
+    initialState: {
+      products:[],
+      loading: false,
+      erroeMessage: null,
+     
+    },
+  
+   
+    extraReducers: {
+      [fetchProducts.pending]: (state) => {
+        state.loading = true;
+      },
+      [fetchProducts.fulfilled]: (state, { payload }) => {
+        console.log(payload);
+        state.products = payload;
+        state.loading = false;
+      },
+      [fetchProducts.rejected]: (state, { payload, error }) => {
+        console.log({ payload, error });
+        return { loading: false, erroeMessage: payload, products: [] };
+      },
+   
+    },
+  });
+  export default productsSlice.reducer;
+
+  
+
+  
