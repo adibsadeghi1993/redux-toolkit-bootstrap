5b4714340047664591424519902d861ffcc89deb
adib sadeghi <adibsadeghi7272@gmail.com>
2021-12-10 11:44:25 +0330

    flex-wrap class was added to radio buttons

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -3,26 +3,18 @@ import React from "react";
 
 const Sort = ({ setSorted, sort }) => {
   return (
-    <div className="d-flex align-items-center mt-5">
-        <span>ترتیب قیمت ها</span>
-      <span className="ms-3">
-        <label className="mb-1 me-1">نزولی</label>
-        <input
-          type="radio"
-          checked={sort === "0"}
-          value="0"
-          onChange={(e) => setSorted(e.target.value)}
-        />
-      </span>
-      <span className="ms-3">
-      <label className="mb-1 me-1">صعودی</label>
-        <input
-          type="radio"
-          checked={sort === "1"}
-          value="1"
-          onChange={(e) => setSorted(e.target.value)}
-        />
-      </span>
+    <div className="d-flex flex-wrap  mt-5">
+        <div><span className="me-2">ترتیب قیمت ها</span></div>
+     
+  
+      <div class="form-check form-check-inline">
+  <input class="form-check-input"  checked={sort === "0"}   onChange={(e) => setSorted(e.target.value)} type="radio" name="inlineRadioOptions" id="inlineRadio1" value="0"/>
+  <label class="form-check-label" for="inlineRadio1">نزولی</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" checked={sort === "1"}   onChange={(e) => setSorted(e.target.value)} type="radio" name="inlineRadioOptions" id="inlineRadio2" value="1"/>
+  <label class="form-check-label" for="inlineRadio2">صعودی</label>
+</div>
     </div>
   );
 };
