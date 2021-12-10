6aff47d16b31d08662aff0612311ca7999cfe6fa
adib sadeghi <adibsadeghi7272@gmail.com>
2021-12-10 10:07:34 +0330

    all products was tranformed from json-server file tp productlist

---
+++ src/productlist/productList.js
@@ -0,0 +1,101 @@
+export const products=[
+    {
+        id:1,
+        title:"ساعت",
+        description:"ساعت مچی مردانه",
+        discount:"50%",
+        mainPrice:180,
+        dPrice:120,
+        src:"imageone",
+        category:"electonic"
+
+    },
+    {
+        id:2,
+        title:"ساعت",
+        description:"ساعت مچی مردانه",
+        discount:"35%",
+        mainPrice:65,
+        dPrice:36,
+        src:"imagetow",
+        category:"electonic"
+
+    },
+    {
+        id:3,
+        title:"ساعت",
+        description:"ساعت مچی مردانه",
+        discount:"12%",
+        mainPrice:49,
+        dPrice:23,
+        src:"imagethree",
+        category:"electonic"
+
+    },
+    {
+        id:4,
+        title:"کفش",
+        description:"کفش اسپرت پیاده روی",
+        discount:"60%",
+        mainPrice:85,
+        dPrice:73,
+        src:"imagefour",
+        category:"electonic"
+
+    },
+    {
+        id:5,
+        title:"کفش",
+        description:"کفش اسپرت پیاده روی",
+        discount:"50%",
+        mainPrice:25,
+        dPrice:15,
+        src:"imagefive",
+        category:"sport"
+
+    },
+    {
+        id:6,
+        title:"کفش",
+        description:"کفش اسپرت پیاده روی",
+        discount:"50%",
+        mainPrice:95,
+        dPrice:43,
+        src:"imagesix",
+        category:"sport"
+
+    },
+    {
+        id:7,
+        title:"لپ تاپ",
+        description:"لپ تاپ اصل با گارانتی",
+        discount:"70%",
+        mainPrice:63,
+        dPrice:33,
+        src:"imageseven",
+        category:"sport"
+
+    },
+    {
+        id:8,
+        title:"لپ تاپ",
+        description:"لپ تاپ اصل با گارانتی",
+        discount:"50%",
+        mainPrice:79,
+        dPrice:26,
+        src:"imageeight",
+        category:"sport"
+
+    },
+    {
+        id:9,
+        title:"لپ تاپ",
+        description:"لپ تاپ اصل با گارانتی",
+        discount:"50%",
+        mainPrice:80,
+        dPrice:65,
+        src:"imagenie",
+        category:"اسپرت"
+
+    }
+]
